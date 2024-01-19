81886a5a50be91648b200c5cf6f7ebc5badee866
Jihoon Lim <121468393+limjihoon99@users.noreply.github.com>
2024-01-19 17:12:13 +0900

    [level 1] Title: 푸드 파이트 대회, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.py
@@ -0,0 +1,10 @@
+def solution(food):
+    
+    eat_food = ''
+    
+    for idx, i in enumerate(food[1:]):
+        eat_food += str(idx + 1) * (i // 2)
+        
+    result = eat_food + "0" + eat_food[::-1]
+    
+    return result
