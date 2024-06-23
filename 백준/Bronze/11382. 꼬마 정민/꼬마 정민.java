88b51b7418b90c89b3f750793f778df6c5a9f70e
Jihoon Lim <121468393+limjihoon99@users.noreply.github.com>
2024-06-23 21:39:57 +0900

    [Bronze V] Title: 꼬마 정민, Time: 216 ms, Memory: 17692 KB -BaekjoonHub

---
+++ 백준/Bronze/11382. 꼬마 정민/꼬마 정민.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public  class  Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        Long a = input.nextLong();
+        Long b = input.nextLong();
+        Long c = input.nextLong();
+
+        System.out.println(a+b+c);
+    }
+}
