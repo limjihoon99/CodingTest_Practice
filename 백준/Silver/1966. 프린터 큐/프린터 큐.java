56526af753bbed04344d0e996a3a6243a0735e33
Jihoon Lim <121468393+limjihoon99@users.noreply.github.com>
2024-11-13 01:46:45 +0900

    [Silver III] Title: 프린터 큐, Time: 132 ms, Memory: 15264 KB -BaekjoonHub

---
+++ 백준/Silver/1966. 프린터 큐/프린터 큐.java
@@ -0,0 +1,62 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int T = Integer.parseInt(br.readLine());
+
+        while (T -- > 0) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+
+            int N = Integer.parseInt(st.nextToken());
+            int M = Integer.parseInt(st.nextToken());
+
+            LinkedList<int[]> list = new LinkedList<>();
+            st = new StringTokenizer(br.readLine());
+
+            for (int i = 0; i < N; i++) {
+                list.offer(new int[] {i, Integer.parseInt(st.nextToken())});
+            }
+
+            int count = 0;
+
+            while (!list.isEmpty()) {
+
+                int[] front = list.poll();
+                boolean isMax = true;
+
+                for (int i = 0; i < list.size(); i++) {
+                    if (front[1] < list.get(i)[1]) {
+                        list.offer(front);
+                        for (int j = 0; j < i; j++) {
+                            list.offer(list.poll());
+                        }
+
+                        isMax = false;
+                        break;
+                    }
+                }
+
+                if (isMax == false) {
+                    continue;
+                }
+
+                count++;
+                if (front[0] == M) {
+                    break;
+                }
+            }
+
+            sb.append(count).append("\n");
+        }
+
+        System.out.println(sb);
+    }
+}
